8179ec58936042c1b61889dc3fa9dce9bd182c06
tupercav <tupercav@gmail.com>
2022-07-04 10:48:19 +0300

    solved

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,11 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
+
 
     private Long nanoTime = System.nanoTime();
 
